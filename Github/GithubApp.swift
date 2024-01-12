e165e198b83dcd9c37de9c6d6c60773cdcb9cb98
fmkiranpoudel <kiran.poudel@fusemachines.com>
2024-01-12 19:42:46 +0545

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by FMI-PC-LT-48 on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
